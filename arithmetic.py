91029363d8d5ad1bd608eb7431c8aefbc2f9ed8d
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 11:48:46 -0700

    square/cube function definitions

--- arithmetic.py
+++ arithmetic.py
@@ -19,10 +19,14 @@ def divide(num1, num2):
     return float(num1) / float(num2)
 
 def square(num1):
-    pass
+    """squaring num1"""
+
+    return num1**2
 
 def cube(num1):
-    pass
+    """cubing num1"""
+
+    return num1**3
 
 def power(num1, num2):
     pass
